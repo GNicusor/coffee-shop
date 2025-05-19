eb71cfe2d5e5a36b0df5684e8786f76f1b58c11c
Ababii Vicu <vicu-marin.ababii@student.upt.ro>
2025-05-19 23:45:17 +0300

    Revert "stock decrement update"
    This reverts commit f9cd5bfd684509fff041138b7a8e9a9d67362d90.

--- src/main/java/server/PaymentController.java
+++ src/main/java/server/PaymentController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -17,51 +16,38 @@ public class PaymentController {
     @Value("${stripe.apiKey}")
     private String stripeApiKey;
 
-    // DTO for incoming items
-    public static class CartItem {
-        public Long   coffeeId;
-        public String name;
-        public Long   unitPrice;
-        public Long   quantity;
-    }
-
-    public static class CreateSessionRequest {
-        public List<CartItem> items;
-    }
-
     @PostMapping("/create-session")
-    public Map<String, String> createSession(@RequestBody CreateSessionRequest req) throws Exception {
+    public Map<String, String> createSession(@RequestBody Map<String, Object> payload) throws Exception {
         Stripe.apiKey = stripeApiKey;
 
-        SessionCreateParams.Builder builder = SessionCreateParams.builder()
-                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
-                .setMode(SessionCreateParams.Mode.PAYMENT)
-                .setSuccessUrl("http://localhost:3000/checkout?success=true")
-                .setCancelUrl("http://localhost:3000/checkout?canceled=true");
+        Long amount = ((Number)payload.get("amount")).longValue();
 
-        for (CartItem item : req.items) {
-            builder.addLineItem(
-                    SessionCreateParams.LineItem.builder()
-                            .setQuantity(item.quantity)
-                            .setPriceData(
-                                    SessionCreateParams.LineItem.PriceData.builder()
-                                            .setCurrency("ron")
-                                            .setUnitAmount(item.unitPrice * 100)
-                                            .setProductData(
-                                                    SessionCreateParams.LineItem.PriceData.ProductData.builder()
-                                                            .setName(item.name)
-                                                            .putMetadata("coffeeId", item.coffeeId.toString())
-                                                            .build()
-                                            )
-                                            .build()
-                            )
-                            .build()
-            );
-        }
+        SessionCreateParams params =
+                SessionCreateParams.builder()
+                        .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+                        .setMode(SessionCreateParams.Mode.PAYMENT)
+                        .setSuccessUrl("http://localhost:3000/checkout?success=true")
+                        .setCancelUrl("http://localhost:3000/checkout?canceled=true")
+                        .addLineItem(
+                                SessionCreateParams.LineItem.builder()
+                                        .setQuantity(1L)
+                                        .setPriceData(
+                                                SessionCreateParams.LineItem.PriceData.builder()
+                                                        .setCurrency("ron")
+                                                        .setUnitAmount(amount*100)
+                                                        .setProductData(
+                                                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                                        .setName("Coffee Order")
+                                                                        .build()
+                                                        )
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build();
 
-        Session session = Session.create(builder.build());
-
-        Map<String, String> response = new HashMap<>();
+        Session session = Session.create(params);
+        Map<String,String> response = new HashMap<>();
         response.put("id", session.getId());
         return response;
     }
